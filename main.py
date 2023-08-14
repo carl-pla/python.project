b54d32287f500273af2c60d818b74a8214d18a1d
FlyingFly3 <Artur.radestock@gmail.com>
2023-08-14 15:05:20 +0200

    check age

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ def check_drive (age):
         print ("Du darfst fahren!")
     elif age < 18: 
         print ("Du darfst nicht fahren!")
-check_drive (17)
-check_drive (18)
+#check_drive (17) 
+#check_drive (18)
+
+# input
+input1 = input("Please enter something ")
+
+# output
+print("You entered"+ input1) 
+
