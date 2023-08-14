fcace9c74e1a9ff36b7ad64867b9254f648866d0
FlyingFly3 <Artur.radestock@gmail.com>
2023-08-14 14:58:45 +0200

    check drive

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print ("Hello Carl")
+def check_drive (age): 
+    if age >= 18: 
+        print ("Du darfst fahren!")
+    elif age < 18: 
+        print ("Du darfst nicht fahren!")
+check_drive (17)
+check_drive (18)
